e18dbf7e4e95291825f4c5e89a1f6441fa7905ae
OxFernando <oxFernando@proton.me>
2023-06-01 16:14:39 +0800

    :lipstick: Update ui

--- src/Top.tsx
+++ src/Top.tsx
@@ -1,7 +1,12 @@
 import s from './Top.module.scss';
 
+import Poster from 'src/asset/poster.jpg';
+
 export default function Top({ children }: any) {
     return <div className={s.wrap}>
+        <div className={s.mask}>
+            <img src={Poster} alt='' />
+        </div>
         <div className={s.container}>
             {children}
         </div>
