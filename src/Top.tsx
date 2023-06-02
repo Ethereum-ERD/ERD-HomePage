cc12ee4dced38dc30cb6100284727be27eff1e68
OxFernando <oxFernando@proton.me>
2023-06-02 11:12:20 +0800

    :wheelchair: Add more video type

--- src/Top.tsx
+++ src/Top.tsx
@@ -4,6 +4,7 @@ import s from './Top.module.scss';
 import Poster from 'src/asset/poster.jpg';
 
 const Video = 'https://erd-fe-storage.s3.amazonaws.com/erd.mp4';
+const VideoM = 'https://erd-fe-storage.s3.amazonaws.com/erd.webm';
 
 export default function Top({ children }: any) {
     const [hidePoster, setHidePoster] = useState(false);
@@ -15,14 +16,16 @@ export default function Top({ children }: any) {
                 loop
                 muted
                 autoPlay
-                src={Video}
                 playsInline
-                preload='auto'
+                preload='none'
                 controls={false}
                 disableRemotePlayback
                 disablePictureInPicture
                 onCanPlayThrough={() => setHidePoster(true)}
-            />
+            >
+                <source src={VideoM} type="video/webm" />
+                <source src={Video} type='video/mp4' />
+            </video>
         </div>
         <div className={s.container}>
             {children}
