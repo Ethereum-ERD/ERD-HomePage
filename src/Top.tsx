2c6e60721a83be5549de70279f30f13bb00e1611
OxFernando <oxFernando@proton.me>
2023-06-02 10:53:12 +0800

    :zap: Fix the flickering problem

--- src/Top.tsx
+++ src/Top.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import s from './Top.module.scss';
 
 import Poster from 'src/asset/poster.jpg';
@@ -5,8 +6,11 @@ import Poster from 'src/asset/poster.jpg';
 const Video = 'https://erd-fe-storage.s3.amazonaws.com/erd.mp4';
 
 export default function Top({ children }: any) {
+    const [hidePoster, setHidePoster] = useState(false);
+
     return <div className={s.wrap}>
         <div className={s.mask}>
+            <img src={Poster} alt='' style={{ opacity: hidePoster ? 0 : 1 }} />
             <video
                 loop
                 muted
@@ -14,10 +18,10 @@ export default function Top({ children }: any) {
                 src={Video}
                 playsInline
                 preload='auto'
-                poster={Poster}
                 controls={false}
                 disableRemotePlayback
                 disablePictureInPicture
+                onCanPlayThrough={() => setHidePoster(true)}
             />
         </div>
         <div className={s.container}>
